ceaae24c4ffb8463d510e59af712187978885b85
PC <burakugras67@gmail.com>
2023-11-12 17:20:20 +0300

    Constructor kullanımı

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -4,8 +4,19 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        MyList<string> isimler=new MyList<string>();
+        MyList<string> isimler = new MyList<string>();
         isimler.Add("Engin");
+
+        Console.WriteLine(isimler.Length);
+
+        isimler.Add("Kerem");
+
+        Console.WriteLine(isimler.Length);
+
+        foreach (var isim in isimler.Items)
+        {
+            Console.WriteLine(isim);
+        }
 
         Console.WriteLine("Hello, World!");
     }
