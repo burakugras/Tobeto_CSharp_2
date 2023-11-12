9e982c418271bdb6cd97ce5c361b2163078fadec
PC <burakugras67@gmail.com>
2023-11-13 00:49:12 +0300

    İş sınıfları ve inheritance

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -4,12 +4,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Musteri musteri1= new Musteri();
-        musteri1.Adi = "Engin";
-        musteri1.Soyadi = "Demiroğ";
-        musteri1.Id = 1;
-        musteri1.TcNo = "22222222";
-        musteri1.MusteriNo = "12345";
-        musteri1.SirketAdi = "?";
+        
     }
 }
--- OOP2/TuzelMusteri.cs
+++ OOP2/TuzelMusteri.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace OOP2
 {
-    internal class Musteri
+    //Coorporate
+    internal class TuzelMusteri:Musteri
     {
-        public int Id { get; set; }
-        public string MusteriNo { get; set; }
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
     }
 }
