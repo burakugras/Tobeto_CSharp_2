b080469428f6f587544616421ba3f82773bb1d6b
PC <burakugras67@gmail.com>
2023-11-12 17:06:15 +0300

    Generics

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using GenericsIntro;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyList<string> isimler=new MyList<string>();
+        isimler.Add("Engin");
+
+        Console.WriteLine("Hello, World!");
+    }
+}
