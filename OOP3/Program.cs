0ea5b708f69206959fb5e46b151f2d75fb6d83e1
PC <burakugras67@gmail.com>
2023-11-13 13:28:34 +0300

    Interface kullanımı

--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
+﻿using OOP3;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        IKrediManager ihtiyacKrediManager =new IhtiyacKrediManager();
+
+        IKrediManager tasitKrediManager =new TasitKrediManager();
+
+        IKrediManager konutKrediManager = new KonutKrediManager();
+
+        BasvuruManager basvuruManager = new BasvuruManager();
+        //basvuruManager.BasvuruYap(konutKrediManager);
+
+        List<IKrediManager> krediler=new List<IKrediManager> { ihtiyacKrediManager,tasitKrediManager};
+        basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
     }
 }
